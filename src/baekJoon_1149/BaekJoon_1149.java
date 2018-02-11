e2e9dd524f61a92765505697474d7949d5c634c6
KOOSANGYOON <nevergiveup936@naver.com>
2018-02-12 01:03:47 +0900

    update algorithm

--- src/baekJoon_1149/BaekJoon_1149.java
+++ src/baekJoon_1149/BaekJoon_1149.java
@@ -1,12 +1,41 @@
 package baekJoon_1149;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /*
  * BaekjoonOnlineJudge#1149 RGB거리
  * https://www.acmicpc.net/problem/1149
  */
 public class BaekJoon_1149 {
-	public static void main(String[] args) {
+	static List<int[]> totalCostList = new ArrayList<int[]> ();
+	
+	static int[] convertToInteger(String[] input) {
+		int[] result = new int[input.length];
 		
+		for (int i = 0; i < input.length; i++) {
+			result[i] = Integer.parseInt(input[i]);
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int totalHouseNum = scan.nextInt();
+		scan.nextLine();
+		
+		for (int i = 0; i < totalHouseNum; i++) {
+			String[] stringCost = scan.nextLine().split(" ");
+			int[] colorCost = convertToInteger(stringCost);
+			totalCostList.add(colorCost);
+		}
+		
+		for (int[] cost : totalCostList) {
+			for (int i : cost) {
+				System.out.println(i);
+			}
+		}
 	}
 }
