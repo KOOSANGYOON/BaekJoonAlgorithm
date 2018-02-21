b20d25d3156f24a17399f51b2263cd44ddcffb60
KOOSANGYOON <nevergiveup936@naver.com>
2018-02-21 12:18:57 +0900

    add baekJoon_10026, not complete.

---
+++ src/baekJoon_10026/BaekJoon_10026.java
@@ -0,0 +1,49 @@
+package baekJoon_10026;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BaekJoon_10026 {
+	static String[][] RGBtable;
+	static boolean[][] isVisited;
+
+	static int inputData() {
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+		RGBtable = new String[size][size];
+		isVisited = new boolean[size][size];
+
+		for (int i = 0; i < size; i++) {
+			String oneLine = scan.next();
+			for (int j = 0; j < oneLine.length(); j++) {
+				RGBtable[i][j] = Character.toString(oneLine.charAt(j));
+				isVisited[i][j] = false;
+			}
+		}
+
+		return size;
+	}
+
+	static void checkZone(int size) {
+		Queue<String> q = new LinkedList<String> ();
+		//		HashMap<String, Boolean> hash = new HashMap<String, Boolean>(); 
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (!isVisited[i][j]) {
+					q.offer(RGBtable[i][j]);
+					isVisited[i][j] = true;
+					
+					String temp = q.poll();
+				}
+			}
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		checkZone(inputData());
+	}
+}
