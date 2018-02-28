f5504d9eaae98972069d91ba064d796ed0451669
KOOSANGYOON <nevergiveup936@naver.com>
2018-02-28 11:00:39 +0900

    update easy questions.

---
+++ src/baekJoon_1000/BaekJoon_1000.java
@@ -0,0 +1,14 @@
+package baekJoon_1000;
+
+import java.util.Scanner;
+
+public class BaekJoon_1000 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a, b;
+		a = scan.nextInt();
+		b = scan.nextInt();
+		scan.close();
+		System.out.println(a+b);
+	}
+}
---
+++ src/baekJoon_1001/BaekJoon_1001.java
@@ -0,0 +1,14 @@
+package baekJoon_1001;
+
+import java.util.Scanner;
+
+public class BaekJoon_1001 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a, b;
+		a = scan.nextInt();
+		b = scan.nextInt();
+		scan.close();
+		System.out.println(a-b);
+	}
+}
