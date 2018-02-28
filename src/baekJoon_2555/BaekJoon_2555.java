a2947d0addbeece11f7b12ea28d495bd190e8f25
KOOSANGYOON <nevergiveup936@naver.com>
2018-02-28 15:25:26 +0900

    add baekJoon_2555.

---
+++ src/baekJoon_2555/BaekJoon_2555.java
@@ -0,0 +1,10 @@
+package baekJoon_2555;
+/*
+ * BaekjoonOnlineJudge#2555 생일 출력하기
+ * https://www.acmicpc.net/problem/2555
+ */
+public class BaekJoon_2555 {
+	public static void main(String[] args) {
+		System.out.println("10/14");
+	}
+}
