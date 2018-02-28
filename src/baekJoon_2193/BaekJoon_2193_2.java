bd265a32e7cbe2b13681524a9938203232ab2df6
KOOSANGYOON <nevergiveup936@naver.com>
2018-02-28 15:49:58 +0900

    update baekJoon_2193, complete.

--- src/baekJoon_2193/BaekJoon_2193_2.java
+++ src/baekJoon_2193/BaekJoon_2193_2.java
@@ -6,39 +6,18 @@ import java.util.Scanner;
  * https://www.acmicpc.net/problem/2193
  */
 public class BaekJoon_2193_2 {
-	static count[] totalCount;
-	
-	static int pinaryNum(int input) {
-		if (input == 0) {
-			return 1;
-		}else if (input == 1) {
-			return 1;
-		}
-		return pinaryNum(input - 2) + pinaryNum(input - 1);
-	}
-	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int input = scan.nextInt();
+		int count = scan.nextInt();
+		
+		long[] pinary = new long[count + 1];
+		pinary[0] = 1;
+		pinary[1] = 1;
+		
+		for (int i = 2; i < pinary.length; i++) {
+			pinary[i] = pinary[i-1] + pinary[i-2];
+		}
+		System.out.println(pinary[count-1]);
 		scan.close();
-		
-		totalCount = new count[input];
-		for (int i = 0; i < totalCount.length; i++) {
-			totalCount[i] = new count();
-		}
-		
-		int result = pinaryNum(input - 1);
-		
-		System.out.println(result);
-	}
-}
-
-class count {
-	private int[][] totalScore = new int[50][1];
-	
-	public count() {
-		for (int i = 0; i < 50; i++) {
-			this.totalScore[i][0] = 0;
-		}
 	}
 }
